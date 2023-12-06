cb4555ae11977a0cc9e01820a8d3795b444fb530
sebastian.velezp <sebastian.velezp@perficient.com>
2023-12-06 13:21:43 -0500

    First Commit

---
+++ src/main/java/guru/springframewoek/spring6auth/Spring6AuthApplication.java
@@ -0,0 +1,13 @@
+package guru.springframewoek.spring6auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6AuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframewoek/spring6auth/Spring6AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframewoek.spring6auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
